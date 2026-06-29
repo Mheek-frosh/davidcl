397105a9900a44959caf569e2831867eac791198
Mike Usidamen <m.usidamen@gmail.com>
2026-06-29 15:34:25 +0100

    fixes

---
+++ src/pages/Shop.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import ProductGrid from '../components/ProductGrid/ProductGrid';
+import './Shop.css';
+
+const Shop = () => {
+  return (
+    <div className="shop-page">
+      <div className="shop-hero">
+        <h1 className="shop-heading">SHOP ALL</h1>
+        <p className="shop-sub">Handcrafted. Limited. Yours.</p>
+      </div>
+      <ProductGrid hideTitle={true} />
+    </div>
+  );
+};
+
+export default Shop;
